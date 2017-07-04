d9a40cab21d2d1d9c1022ef748628c5f51398737
J Sandate <mrharisu@gmail.com>
2017-07-04 12:43:22 -0700

    add migrations and seeds

--- db/seeds/restaurants_seed_file.js
+++ db/seeds/restaurants_seed_file.js
@@ -1,13 +1,29 @@
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('restaurants').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
-        { colName: 'rowValue1'},
-        {colName: 'rowValue2'},
-        {colName: 'rowValue3'}
+      return knex('restaurants').insert([
+        { user_id: 1,
+          restaurant_name: 'fired pie',
+          location: 'avondale, az',
+          date_visited: 01/22/2016,
+          review: 'I enjoy the atmosphere and speedy service',
+          visited: true
+        },
+        { user_id: 2,
+          restaurant_name: 'the habit',
+          location: 'tempe, az',
+          date_visited: 04/19/2016,
+          review: 'Tasty hamburger, didnt disappoint',
+          visited: true
+        },
+        { user_id: 3,
+          restaurant_name: 'McDonalds',
+          location: 'phoenix, az',
+          visited: false
+        }
       ]);
     });
 };
